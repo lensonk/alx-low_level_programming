8397f13986fc9e32e310402e379ac3534a0bc105
lesnonk <lensonk@gmail.com>
2022-12-11 11:41:23 -0800

    eddited documentation

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 /**
- * main function-print a string
+ * main-entry point
+ *
+ * description: prints 'with proper grammer, but the outcome is a piece of art
+ *
+ * Return: always return 0
  */
 int main(void)
 {
@@ -8,8 +12,6 @@ int main(void)
 	 * print with proper grammer, but the outcome is a piece of art
 	 * followed by a new line
 	 * using printf function
-	 *
-	 * Return: print the string 'with proper grammer, but the outcome is a piece of art'
 	 */
 	printf("with proper grammer, but the outcome is a piece of ar,\n");
 	return (0);
