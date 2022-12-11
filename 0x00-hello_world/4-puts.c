6ff49a8b179388b86ded8ebab9dd23fb9e975fb4
lesnonk <lensonk@gmail.com>
2022-12-11 06:27:39 -0800

    puts string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 int main(void)
 {
-I	/*String initialization*/
-char PUZZLE[]  = ""programming is like building a multilingual puzzle";
+	/*String initialization*/
+puts("\"programming is like building a multilingual puzzle");
       /* Print to std output */
-puts(PUZZLE);
 
-	return 0;
-
+return (0);
 }
