57975cabf33f6b02873b7ecdd0f5cf24e969f1fa
lesnonk <lensonk@gmail.com>
2022-12-11 08:03:38 -0800

    added new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,6 @@
 int main()
 {
 	/* String initialization */
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"programming is like building a multilingual puzzlie\n");
 	return (0);
 }
