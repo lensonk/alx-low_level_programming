b7e3ca679dbc42c6f6d9f413b1426d943b9911f6
lesnonk <lensonk@gmail.com>
2022-12-11 11:25:48 -0800

    edited documentation

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,6 +8,8 @@ int main(void)
 	 * print with proper grammer, but the outcome is a piece of art
 	 * followed by a new line
 	 * using printf function
+	 *
+	 * Return: print the string 'with proper grammer, but the outcome is a piece of art'
 	 */
 	printf("with proper grammer, but the outcome is a piece of ar,\n");
 	return (0);
