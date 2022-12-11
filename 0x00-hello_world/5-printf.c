48dd1ef7f10d888f4a49470c261b53b9983c4c5e
lesnonk <lensonk@gmail.com>
2022-12-11 10:53:20 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-main(void)
+
+int main(void)
 {
 	/**
 	 * print with proper grammer, but the outcome is a piece of art
 	 * followed by a new line
-	 * usin printf function
-	 * should return 0
+	 * using printf function
 	 */
 	printf("with proper grammer, but the outcome is a piece of art\n");
 	return (0);
