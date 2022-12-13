7eb922f1cff9738af3dcbad06b4f8c4906c9db7f
lesnonk <lensonk@gmail.com>
2022-12-13 10:02:25 -0800

    editedd printf format

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -20,15 +20,15 @@ int main(void)
 	{
 	if (n > 0)
 	{
-	printf("%d", n); "is positive\n";
+	printf("%d is positive\n", n);
 	}
 	else if (n < 0)
 	{
-	printf("%d", n); "is negative\n";
+	printf("%d is negtive\n", n);
 	}
 	else
 	{
-	printf("%d", n); "is zero\n";
+	printf("%d is zero\n", n);
 	}
 
 	return (0);
