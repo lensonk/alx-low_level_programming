d33d304bdb5aab7b314642ca07ae964eb9817d2a
lesnonk <lensonk@gmail.com>
2022-12-13 16:05:41 -0800

    a to z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ *
+ * Description:print lowercase alphabets using putchar
+ *
+ * Return:always 0
+ */
+int main(void)
+{
+	char i;
+
+	/* assignment*/
+
+	for (i = 'a'; i <= 'z'; i++)
+
+	 putchar(i);
+
+	return (0);
+}
