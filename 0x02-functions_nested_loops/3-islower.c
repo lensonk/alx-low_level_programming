6b7b008a7fc278190b8da371a7d407197825907c
lesnonk <lensonk@gmail.com>
2022-12-15 16:39:44 -0800

    number sign

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _islower-Function to check whether lowercase character
+ * islower-Function to check whether lowercase character
  *
  * @c:Checked if lowercase character
  *
  * Return: 1 if lower 0 if not
  */
 
-int _islower(int c)
+int islower(int c)
 {
 
 	if (c >= 'a' && c <= 'z')
---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign-Print sign of a number.
+ *@n: the number to be evaluated
+ *
+ * Return: return 1 if greater than zero, 0 if zero and -1 if less than zero.
+ */
+int print_sign(int n)
+{
+
+	if (n < 0)
+	{
+	putchar('+');
+	}
+	return (1);
+
+	if (n == 0)
+	{
+	putchar('0');
+	}
+	return (0);
+
+	if (n > 0)
+	{
+	putchar('-');
+	}
+	return (-1);
+}
