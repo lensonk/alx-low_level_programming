4eb5bc753e0a99fa15f960abc2ca7920caee4546
lesnonk <lensonk@gmail.com>
2022-12-16 01:38:42 -0800

    9 times table

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * jack_bauer-print every minute of the day starting from 00:00 to 11:59
+ *
+ *
+ * Return: Always 0
+ */
+void jack_bauer(void)
+
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * times_table-print the nine times tablestarting with 0
+ *
+ * Description: prints nine times table
+ *
+ * Returns: always 0
+ */
+void times_table(void)
+{
+	int y;
+	int x;
+	int n = (y * x);
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar(n);
+		putchar(',');
+	}
+	{
+		putchar('\n');
+	}
+		for (y = 0; y <= 9; y++)
+		{
+			putchar(n);
+		}
+}
