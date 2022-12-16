bf809202b7af0284d22c520af7273ef193d85f9b
lesnonk <lensonk@gmail.com>
2022-12-15 17:22:43 -0800

    edited condition

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,7 +9,7 @@
 int print_sign(int n)
 {
 
-	if (n < 0)
+	if (n > 0)
 	{
 	_putchar('+');
 	}
@@ -21,7 +21,7 @@ int print_sign(int n)
 	}
 	return (0);
 
-	if (n > 0)
+	if (n < 0)
 	{
 	_putchar('-');
 	}
