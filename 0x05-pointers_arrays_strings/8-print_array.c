47a4e40ab5b9543f0e2fd053fa79eecc9bd5a77b
lesnonk <lensonk@gmail.com>
2022-12-20 18:57:10 -0800

    array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,6 +2,7 @@
 
 /**
  * print_array - print n elements of array
+ * @a: integer value to be printed
  * @n: integert rep number of array elements
  */
 
@@ -11,5 +12,11 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 
+	if (i != n - 1)
+
 		printf("%d, ", a[i]);
+	else
+		printf("%d", a[i]);
+
+		printf('\n');
 }
