5b828f4333fb56dd8398268565ec3ef2dd94e8d7
lesnonk <lensonk@gmail.com>
2022-12-21 17:42:07 -0800

    print characters in a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,12 +9,12 @@ void puts2(char *str)
 {
 	int n;
 
-	for (n = 0; n != '\0'; n++)
-		if (n % 2 == 0)
-	{
+	for (n = 0; str[n] != '\0'; n++)
 		putchar(str[n]);
-	
+	{
+
 		putchar('\n');
 	}
+
 }
 
