8b420d947c53bbc49155728e8b351607232965b3
lesnonk <lensonk@gmail.com>
2022-12-23 15:01:52 -0800

    apend string

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,26 +3,28 @@
 /**
  * *_strcat - append the source string at the end c of the destination string
  * @src: source string to be appended
- * @ dest: destination string to append to
- * Return: Return *dest
+ * @dest: destination string to append to
+ * Return: Return dest
  */
 
 char *_strcat(char *dest, char *src)
 {
-	char *i = dest;
+	*dest = dest[0];
 
 	while (*dest != '\0')
 	{
 		dest++;
 	}
-			while (*src != '\0')
-			{
-			*dest = *src;
 
-			dest++;
-			src++;
-			}
+	while (*src != '\0')
+		{
+
+		*dest = *src;
+		src++;
+		dest++;
+		}
 
 		*dest = '\0';
-		return (i);
+
+		return (dest);
 }
