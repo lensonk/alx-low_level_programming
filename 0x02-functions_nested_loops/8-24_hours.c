101db369e0d138e897c63b372319b0ecdbbc0c9b
lesnonk <lensonk@gmail.com>
2022-12-26 08:08:01 -0800

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,4 +7,29 @@
  * Return: Always 0
  */
 void jack_bauer(void)
+{
+	int m;
+	int h = 0;
 
+	while (h < 23)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			if (m <= 9)
+			{
+			_putchar(h);
+			putchar(':');
+			_putchar('0');
+			_putchar(m);
+			}
+			else
+			{
+			_putchar(h);
+			putchar(':');
+			_putchar(m);
+			}
+			putchar('\n');
+		}
+	}
+		h++;
+}
