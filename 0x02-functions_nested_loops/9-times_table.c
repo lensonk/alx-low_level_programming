b832517de42ecc73ca9d1b362b04fc961dddb582
lesnonk <lensonk@gmail.com>
2022-12-26 14:59:07 -0800

    initialized y & x

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,13 +13,13 @@ void times_table(void)
 	int x;
 	int n;
 
-		n = x * y;
 	for (x = 0; x <= 9; x++)
 	{
 		_putchar('0');
 	
 		for (y = 1; y <= 9; y++)
 	{
+		n = x * y;
 	_putchar((n / 10) + '0');
 	_putchar((n % 10) + '0');
 	}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,2 @@
 # A program that print _putchar followed by a new line
+#
