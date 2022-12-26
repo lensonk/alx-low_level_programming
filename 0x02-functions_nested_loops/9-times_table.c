9b5d843a42daad5e326740cef398084d625e53b5
lesnonk <lensonk@gmail.com>
2022-12-26 15:49:48 -0800

    edited if statement

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,15 +16,27 @@ void times_table(void)
 	for (x = 0; x <= 9; x++)
 	{
 		_putchar('0');
-	
+
+		if (x < 9)
+		{
+		_putchar(',');
+		}
+
 		for (y = 1; y <= 9; y++)
-	{
+		{
 		n = x * y;
-	_putchar((n / 10) + '0');
-	_putchar((n % 10) + '0');
-	}
-	if (x < 9)
-	_putchar(',');
+
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+		}
+
+		if (y < 9)
+		{
+		_putchar(',');
+		}
+		{
+		putchar('\n');
+		}
 	}
 
 }
