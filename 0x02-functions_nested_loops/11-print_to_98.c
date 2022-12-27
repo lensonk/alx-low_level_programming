d44f77b73af6ec500c32070dfce16898b70bb4f4
lesnonk <lensonk@gmail.com>
2022-12-27 11:39:15 -0800

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,3 +7,19 @@
  */
 
 void print_to_98(int n)
+{
+	int i;
+
+	if (n < 98)
+	{
+		for (i = n; i >= 0 && i <= 98; i++)
+			putchar(i % 10 + '0');
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+			putchar(i % 10 + '0');
+	}
+	putchar('\n');
+}
+
