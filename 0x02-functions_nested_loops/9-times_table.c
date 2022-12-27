e8151f3885168ddb23dafa44227db428baad1572
lesnonk <lensonk@gmail.com>
2022-12-26 20:04:35 -0800

    edited putchar print value

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,13 +13,13 @@ void times_table(void)
 	int x;
 	int n;
 
-	for (x = 0; x <= 9; x++)
+	for (x = 1; x <= 9; x++)
 	{
 		for (y = 0; y <= 9; y++)
 		{
 		n = x * y;
 
-		_putchar(n / 10 + '0');
+		_putchar(n / 10);
 		_putchar(n % 10 + '0');
 		if (y < 9)
 		_putchar(',');
