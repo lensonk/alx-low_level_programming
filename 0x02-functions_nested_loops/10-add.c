3e7b7f9a7d12949debe6700249e8106e9368b3d4
lesnonk <lensonk@gmail.com>
2022-12-26 21:12:42 -0800

    add two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns result
+ * @a: integer parameter
+ * @b: integer parameter
+ * Return: Result
+ */
+
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,2 +1,11 @@
 # A program that print _putchar followed by a new line
 #
+#
+#
+#
+#
+#
+#
+# Prints every minute of the day
+# Prints nine times table
+# Adds two integers and returns result
