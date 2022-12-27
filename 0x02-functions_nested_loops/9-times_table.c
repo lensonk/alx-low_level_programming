a98fee949c543afd4dd6ee403c27d9dafb3db5bc
lesnonk <lensonk@gmail.com>
2022-12-26 16:07:01 -0800

    tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,26 +13,26 @@ void times_table(void)
 	int x;
 	int n;
 
-	for (x = 0; x <= 9; x++)
+	for (x = 0; x < 9; x++)
 	{
 		_putchar('0');
 		if (x < 9)
 		_putchar(',');
 
-		for (y = 1; y <= 9; y++)
+		for (y = 1; y < 9; y++)
 		{
 		n = x * y;
 
 		_putchar((n / 10) + '0');
 		_putchar((n % 10) + '0');
-		}
 
-		if (y < 9)
+		if (x < 9)
 		{
 		_putchar(',');
 		}
 		{
 		putchar('\n');
+		}
 		}
 	}
 
