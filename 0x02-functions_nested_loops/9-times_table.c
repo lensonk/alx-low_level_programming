2d63fe8ea541eaa8f519a3646e4207517f4a806f
lesnonk <lensonk@gmail.com>
2022-12-27 09:05:56 -0800

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -25,18 +25,11 @@ void times_table(void)
 		n = y * x;
 
 		if (n <= 9)
-		{
 			_putchar(' ');
-
-		}
-
 		else
-		{
 		_putchar(n / 10 + '0');
 		_putchar(n % 10 + '0');
 		}
 		_putchar('\n');
-		}
-
 	}
 }
