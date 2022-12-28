48d42d174d02ba6222290351c41d72306e20c1c1
lesnonk <lensonk@gmail.com>
2022-12-27 18:22:16 -0800

    n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,7 +11,7 @@ void print_times_table(int n)
 	int t;
 	int j;
 
-	while (n >= 0 && n < 15)
+	if(n >= 0 && n < 15)
 	{
 
 	for (j = 0; j <= n; j++)
@@ -30,6 +30,5 @@ void print_times_table(int n)
 	}
 	putchar('\n');
 	}
-	n++;
 	}
 }
