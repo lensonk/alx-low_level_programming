060986290a7ada10a13594ae6ee7082998db785a
lesnonk <lensonk@gmail.com>
2022-12-27 17:27:16 -0800

    n times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_times_table - print n times table starting with 0
+ *@n: number of times table to print
+ */
+
+void print_times_table(int n)
+{
+	int i;
+	int t;
+
+	for (n = 0; n >= 0 && n <= 15; n++)
+	{
+		putchar('0');
+	for (i = 1; i >= 1 && i <= 15; i++)
+		putchar(',');
+		putchar(' ');
+
+		t = n * i;
+
+		putchar(t / 10 + '0');
+		putchar(t % 10 + '0');
+	}
+	putchar('\n');
+}
+
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -9,3 +9,5 @@
 # Prints every minute of the day
 # Prints nine times table
 # Adds two integers and returns result
+# Prints all natural numbers from nth number to 98
+# Prints n times table between 0 and 15
