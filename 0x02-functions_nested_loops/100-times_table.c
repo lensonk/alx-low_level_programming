2815aefcf549a8811eeaa0cb2ff115087303a0be
lesnonk <lensonk@gmail.com>
2022-12-27 17:39:28 -0800

    n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,7 +13,9 @@ void print_times_table(int n)
 	for (n = 0; n >= 0 && n <= 15; n++)
 	{
 		putchar('0');
+
 	for (i = 1; i >= 1 && i <= 15; i++)
+	{
 		putchar(',');
 		putchar(' ');
 
@@ -23,5 +25,5 @@ void print_times_table(int n)
 		putchar(t % 10 + '0');
 	}
 	putchar('\n');
+	}
 }
-
