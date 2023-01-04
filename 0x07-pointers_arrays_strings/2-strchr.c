da35a2b97a4a05ed78e13c9e725b08bf6112efb9
lesnonk <lensonk@gmail.com>
2023-01-04 15:25:57 -0800

    locate character c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,14 +11,12 @@
 char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
+	{
 		if (*s != c)
 			s++;
-	if (*s == c)
-		{
-			return (s);
-		}
-	else
-	{
-			return (NULL);
 	}
+	if (*s == c)
+		return (s);
+	else
+		return (NULL);
 }
