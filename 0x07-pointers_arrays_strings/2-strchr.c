3a835345af7d3d3b42fd729cf956d3bbff26ab72
lesnonk <lensonk@gmail.com>
2023-01-04 13:57:34 -0800

    locate char

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strchr-locates a character in a string
+ * @*s:character pointer
+ * @c:character to be located
+ * @s: string pointed to
+ * Return: pointer to c
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != c)
+	s++;
+
+		if (*s == c)
+		{
+			return (s);
+		}
+		else
+		{
+			return (NULL);
+		}
+}
