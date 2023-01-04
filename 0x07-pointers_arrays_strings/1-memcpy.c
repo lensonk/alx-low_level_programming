d02cdc6d59f3b9c2eaa57d42a58515a4da787d91
lesnonk <lensonk@gmail.com>
2023-01-04 12:29:47 -0800

    mem copy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,14 +11,10 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	unsigned int j;
 
-	for (i = 0; i < n; i++)
-	{
-		for (j = 0; j < n; j++)
+		for (i = 0; i < n; i++)
 		{
-		dest[j] = src[i];
+		dest[i] = src[i];
 		}
-	}
 	return (dest);
 }
