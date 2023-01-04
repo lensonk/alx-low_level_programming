18323bd8e7e5c58b8f6021184e6f08d5c5ab823e
lesnonk <lensonk@gmail.com>
2023-01-04 14:16:18 -0800

    locate char

--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,2 +1,3 @@
 # fills memory with constant byte
 # copies memory area
+# locates character c in string s
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,4 +5,5 @@
 int _putchar(int);
 char *_memset(char *s, char b, int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 #endif
