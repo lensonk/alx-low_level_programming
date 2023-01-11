3f6c3ca0a6e746875c8d34d4160b2bf3cbff95c6
lesnonk <lensonk@gmail.com>
2023-01-11 14:00:55 -0800

    prefix bytes

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -17,7 +17,7 @@ unsigned int _strspn(char *s, char *accept)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		{
-		if (s[i] == accept[j])
+		if (s[i] != 32)
 			for (j = 0; accept[j] != '\0'; j++)
 			{
 				if (s[i] == accept[j])
