e6c77e75013bae2ce75f88d441e3e450bced980a
lesnonk <lensonk@gmail.com>
2023-01-24 13:40:29 -0800

    print name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,4 @@
-#ifndef _FUNTION_POINTERS_H_
-#define _FUNTION_POINTERS_H_
-void print_name(char *name, void (*f)(char*c));
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
+void print_name(char *name, void (*f)(char *c));
 #endif
