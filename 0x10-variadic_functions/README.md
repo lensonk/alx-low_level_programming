bbe50b0ab05e1c3d64c1ad01f225067330f97195
lesnonk <lensonk@gmail.com>
2023-01-25 15:07:41 -0800

    print strings

--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -1,2 +1,3 @@
 # Returns the sum of all its parameters
 # Prints numbers followed by a new line
+# prints strings followed by a new line
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,4 +5,5 @@
 #include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *seperator, const unsigned int n, ...);
+void print_strings(const char *seperator, const unsigned int n, ...);
 #endif
